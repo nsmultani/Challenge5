7d61eecad7de72d61d7e390207cf4e89a84a40e8
nsmultani <navjotsingh5@academic.rrc.ca>
2025-08-16 15:20:34 +0530

    Update views to show categories and fix N+1 query issue

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
-#product.rb class
-
 class Product < ApplicationRecord
+  belongs_to :category
+  
   validates :title, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :stock_quantity, presence: true, numericality: { greater_than_or_equal_to: 0, only_integer: true }
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,26 @@
-Product.destroy_all
+require "csv"
 
-676.times do
+Product.destroy_all
+Category.destroy_all
+
+csv_file = Rails.root.join('db/products.csv')
+csv_data = File.read(csv_file)
+
+products = CSV.parse(csv_data, headers: true)
+
+products.each do |product_row|
+  category_name = product_row['category']
+  
+  category = Category.find_or_create_by(name: category_name)
+  
   Product.create!(
-    title: Faker::Commerce.product_name,
-    description: Faker::Lorem.paragraph(sentence_count: 3),
-    price: Faker::Commerce.price(range: 1.0..1000.0, as_string: false),
-    stock_quantity: Faker::Number.between(from: 0, to: 100)
+    title: product_row['name'],  
+    description: product_row['description'],
+    price: product_row['price'].to_f,
+    stock_quantity: product_row['stock quantity'].to_i,  \
+    category: category
   )
 end
 
+puts "Created #{Category.count} categories"
 puts "Created #{Product.count} products"
