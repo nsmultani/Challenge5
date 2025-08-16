1154f86219c04daa965c3450e23541933aff478b
nsmultani <navjotsingh5@academic.rrc.ca>
2025-08-16 14:52:51 +0530

    Add Product show action, view, route, and link from index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :products, only: [:index]
+  resources :products, only: [:index, :show]
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
