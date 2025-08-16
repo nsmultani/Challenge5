cc232dadb82dad3e7d15038fef50a7ff9ab94cad
nsmultani <navjotsingh5@academic.rrc.ca>
2025-08-16 14:30:52 +0530

    Generate Product model with title, description, price, and stock_quantity

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,4 @@
-# app/models/product.rb
 class Product < ApplicationRecord
-  belongs_to :category
-  
   validates :title, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :stock_quantity, presence: true, numericality: { greater_than_or_equal_to: 0, only_integer: true }
